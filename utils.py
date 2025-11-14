9fde73d9ca05aca3eb9cc808b64db36e3e3ef9c4
Saloninaik12 <155801599+Saloninaik12@users.noreply.github.com>
2025-11-14 22:53:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,72 +1,71 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.applications import (
-    ResNet50, 
-    VGG16, 
-    MobileNetV2, 
-    resnet50, 
-    vgg16, 
-    mobilenet_v2
+    resnet50, vgg16, mobilenet
 )
 from tensorflow.keras.preprocessing import image
-import cv2
-import matplotlib.pyplot as plt
 
-# ---- Load available models ----
+# -----------------------
+# Model Loader
+# -----------------------
 def load_model(model_name):
     if model_name == "ResNet50":
-        return ResNet50(weights="imagenet")
+        model = resnet50.ResNet50(weights='imagenet')
+        preprocess = resnet50.preprocess_input
+        decode = resnet50.decode_predictions
+
     elif model_name == "VGG16":
-        return VGG16(weights="imagenet")
-    elif model_name == "MobileNetV2":
-        return MobileNetV2(weights="imagenet")
+        model = vgg16.VGG16(weights='imagenet')
+        preprocess = vgg16.preprocess_input
+        decode = vgg16.decode_predictions
+
+    elif model_name == "MobileNet":
+        model = mobilenet.MobileNet(weights='imagenet')
+        preprocess = mobilenet.preprocess_input
+        decode = mobilenet.decode_predictions
+
     else:
-        raise ValueError("Unknown model name!")
+        raise ValueError("Unknown model selected")
 
-# ---- Preprocess image for selected model ----
-def preprocess_for_model(img, model_name):
+    return model, preprocess, decode
+
+
+# -----------------------
+# Preprocess Image
+# -----------------------
+def preprocess_for_model(img, preprocess_fn):
     img = img.resize((224, 224))
-    arr = image.img_to_array(img)
-    arr = np.expand_dims(arr, axis=0)
+    img_arr = image.img_to_array(img)
+    img_arr = np.expand_dims(img_arr, axis=0)
+    img_arr = preprocess_fn(img_arr)
+    return img_arr
 
-    if model_name == "ResNet50":
-        arr = resnet50.preprocess_input(arr)
-    elif model_name == "VGG16":
-        arr = vgg16.preprocess_input(arr)
-    elif model_name == "MobileNetV2":
-        arr = mobilenet_v2.preprocess_input(arr)
 
-    return arr
-
-# ---- Decode predictions based on model ----
-def decode_predictions(preds, model_name):
-    if model_name == "ResNet50":
-        return resnet50.decode_predictions(preds, top=5)[0]
-    elif model_name == "VGG16":
-        return vgg16.decode_predictions(preds, top=5)[0]
-    elif model_name == "MobileNetV2":
-        return mobilenet_v2.decode_predictions(preds, top=5)[0]
-
-# ---- Grad-CAM Heatmap ----
-def make_gradcam_heatmap(img_array, model, layer_name=None):
-    if layer_name is None:
-        layer_name = model.layers[-3].name  # pick last conv layer
-    
+# -----------------------
+# GradCAM (No OpenCV)
+# Pure TensorFlow Implementation
+# -----------------------
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name):
     grad_model = tf.keras.models.Model(
-        [model.inputs], 
-        [model.get_layer(layer_name).output, model.output]
+        [model.inputs],
+        [model.get_layer(last_conv_layer_name).output, model.output]
     )
 
     with tf.GradientTape() as tape:
-        conv_output, predictions = grad_model(img_array)
-        pred_index = np.argmax(predictions[0])
-        loss = predictions[:, pred_index]
+        conv_outputs, predictions = grad_model(img_array)
+        pred_index = tf.argmax(predictions[0])
+        pred_output = predictions[:, pred_index]
 
-    grads = tape.gradient(loss, conv_output)
+        grads = tape.gradient(pred_output, conv_outputs)
+
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-    conv_output = conv_output[0]
 
-    heatmap = np.dot(conv_output, pooled_grads.numpy())
+    conv_outputs = conv_outputs[0]
+    heatmap = tf.reduce_sum(
+        tf.multiply(pooled_grads, conv_outputs), axis=-1
+    )
+
     heatmap = np.maximum(heatmap, 0)
-    heatmap /= np.max(heatmap)
+    heatmap /= heatmap.max()
 
     return heatmap
